0c14fbedfd09e6576fbf6e996e17cb30c1eaad4a
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-05-21 16:30:00 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -69,6 +69,7 @@ df['surface_terrain'] = df['surface_terrain'].fillna(0)
 df['price_m2'] = df['valeur_fonciere']/df['surface_relle_bati']
 df.loc[df['surface_terrain'] >= 1, 'price_m2'] = df['valeur_fonciere']/(df['surface_relle_bati']+ np.log(df.surface_terrain))
 df.loc[df['surface_terrain'] <= 1, 'price_m2'] = df['valeur_fonciere']/df['surface_relle_bati']
+df = df[df['price_m2'] > 0]
 
 df['date_mutation']= pd.to_datetime(df['date_mutation'])
 df['year'] = df['date_mutation'].dt.year
