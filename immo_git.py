6f90275499e198634b39cd27ffeb462a0afdf51d
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-07-19 23:08:55 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -229,14 +229,14 @@ st.subheader('Carte des ventes :')
 df_map = df_w_o.filter(['lat','lon'], axis=1)
 df_hmap=df_map.value_counts(['lat', 'lon']).reset_index(name='Transactions')
 #df_mapg=df_map.groupby(['lat','lon']).B.agg('count').to_frame('count').reset_index()
-#st.dataframe(df_map)
+st.dataframe(df_hmap)
 #figmap = px.density_mapbox(df_map,lat=df_map.lat, lon=df_map.lon, radius-1, center=dict (lat=lat1, lon=lon1), zoom=1.5, mapbox_style="stamen-terrain")
 #figmap = px.scatter_geo(df_map, lat='Lat', lon='Long')
 
-figmap = px.density_mapbox(df_hmap, lat='lat', lon='lon', z='Transactions', radius=10,
-                        center=dict(lat=lat1, lon=lon1), zoom=10,
-                        mapbox_style="stamen-terrain")
-st.plotly_chart(figmap)
+#figmap = px.density_mapbox(df_hmap, lat='lat', lon='lon', z='Transactions', radius=10,
+                        #center=dict(lat=lat1, lon=lon1), zoom=10,
+                        #mapbox_style="stamen-terrain")
+#st.plotly_chart(figmap)
 st.balloons()
 st.stop()
 
