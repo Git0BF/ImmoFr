12d65e0cc398e648cf0892e37f627903a15690f9
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-06 14:46:51 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -152,7 +152,7 @@ st.subheader('Evolution des ventes par années :')
 
 
 #st.dataframe(df_year)
-chartdist=px.line(df_year, x=df_year.index, y=['Appartement','Maison'], log_y=True)
+chartdist=px.bar(df_year, x=df_year.index, y=['Appartement','Maison'], barmode='group')
 
 st.plotly_chart(chartdist)
   
