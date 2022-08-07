e1a1d81b95a36d5581f7c4e75d18d597dfab76bf
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-07 13:52:00 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -165,7 +165,8 @@ figpie = px.pie(df_pie, values='Nbr_de_ventes', names='type_local', title=None)
 st.plotly_chart(figpie)
 
 st.subheader('Evolution des ventes par années :')
-figdist = px.histogram(df_year, x=df_year.index)
+figdist = px.histogram(df_year, x=df_year.index, color=df_year.columns)
+#figdist = px.histogram(df_year, x=df_year.index)
 #chartdist=px.line(df_year, x=df_year.index, y=['Appartement','Maison'], barmode='group')
 
 st.plotly_chart(figdist)
