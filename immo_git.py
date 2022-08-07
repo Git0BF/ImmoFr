f423a8918eb8c4f14fb6b2c9c9512ad72c08c71c
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-08-07 14:11:22 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -162,11 +162,12 @@ figpie = px.pie(df_pie, values='Nbr_de_ventes', names='type_local', title=None)
 st.plotly_chart(figpie)
 
 st.subheader('Evolution des ventes par années :')
+  
 #figdist = px.histogram(df_year, x=df_year.index, color=df_year.columns)
 #figdist = px.histogram(df_year, x=df_year.index)
-#figdist=px.line(df_year, x=df_year.index, y=['Appartement','Maison'], barmode='group')
+figdist=px.bar(df_year, x=df_year.index, y=df.columns, barmode='group')
 
-st.dataframe(df_year)
+st.plotly_chart(figdist)
  
 # Separate local types and graph the mean and median price evolution over time.  
 median_ap=median[median['type_local'].str.contains('Maison') == False]
