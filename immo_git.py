37e10917777c7906abaaab41e9ed0a4f44b6a720
Git0BF <105969960+Git0BF@users.noreply.github.com>
2022-09-22 15:36:10 +0200

    Update immo_git.py

--- immo_git.py
+++ immo_git.py
@@ -241,8 +241,8 @@ df_surf_dist1 = df_surf_dist1.rename(columns={'left':'index'}).set_index('index'
 df_surf_dist1.rename(columns = {'surface_relle_bati':'Ventes'}, inplace = True)
 col2.subheader('Distribution Qte/Surf')
 #col2.bar_chart(df_surf_dist1)
-#fig = px.bar(df_surf_dist1,x = 'index',y = 'ventes')
-st.dataframe(df_surf_dist1)
+fig = px.bar(df_surf_dist1,x = 'index',y = 'ventes')
+st.plotly_chart(fig)
 
 df_price_dist1=df_price_dist
 df_price_dist1['left'] = df_price_dist['range'].array.left
